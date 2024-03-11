31b7fd9e13c3ace18faede9529bf247609ebb76d
John Richard Espedal <117681128+Johnricharde@users.noreply.github.com>
2024-03-11 17:19:21 +0100

    Initial commit

---
+++ tower_of_hanoi.py
@@ -0,0 +1,16 @@
+NUMBER_OF_DISKS = 3
+number_of_moves = 2**NUMBER_OF_DISKS - 1
+rods = {
+    'A': list(range(NUMBER_OF_DISKS, 0, -1)),
+    'B': [],
+    'C': []
+}
+
+def move(n, source, auxiliary, target):
+    # display starting configuration
+    print(rods)
+    for i in range(number_of_moves):
+        print(i)
+
+# initiate call from source A to target C with auxiliary B
+move(NUMBER_OF_DISKS, 'A', 'B', 'C')
